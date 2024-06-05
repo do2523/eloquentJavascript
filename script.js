011b1ed9a2e0a8aa231596962588c986d65e4123
do262431@gmail.com <do262431@gmail.com>
2024-06-05 18:39:51 -0400

    chessBoard

---
+++ script.js
@@ -0,0 +1,22 @@
+function chessBoard(length, width){
+    let lineType = 2
+    let lineA = ""
+    let lineB = ""
+
+    for (let j = 0; j < width; j++){
+        lineA += "#"
+        lineA += " "
+        lineB += " "
+        lineB += "#"
+    }
+
+    for (let i = 0; i < length; i++){
+            if (x % 2 == 0){
+            console.log(holder1)
+            }
+            else{
+                console.log(holder2)
+            }
+            lineType += 1
+    }
+}
