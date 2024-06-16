7b62b62a44bddb189744bc60c4dcd2bd2d93464d
Adam Standish <devadamstandish@gmail.com>
2024-06-15 20:03:10 -0400

    chess-board svg

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-counter = 0
-function countChar(str, c){
-    for (let i = 0; i < str.length; i++){
-        if (str[i] == c){
-            counter += 1
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = 0;
+    function countChar(str, c){
+        for (let i = 0; i < str.length; i++){
+            if (str[i] == c){
+                counter += 1;
+            }
+            console.log(countChar("Javascript", "a"));
         }
+        return counter;
     }
-    return counter;
 
-}
-
-console.log(countChar("Javascript", "a"))
+})
